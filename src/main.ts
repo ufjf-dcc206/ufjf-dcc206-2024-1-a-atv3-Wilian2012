15861d5049baacddf95d68abdd67100880bb4cfb
wilian santos <14809032647@estudante.ufjf.br>
2024-09-17 21:50:35 -0300

    busca pokemon

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const END_POINT = "https://pokeapi.co/api/v2/pokemon/1";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+fetch(END_POINT)
+  .then(data => data.json())
+  .then(pkm => {
+
+    document.querySelector<HTMLDivElement>('#app')!.innerHTML = JSON.stringify(pkm);
+
+  }
+
+  );
